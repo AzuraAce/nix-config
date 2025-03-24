0880845db30d89cdfe99c19c1c916cc60845bc14
AzuraAce <oliverjbmub@gmail.com>
2025-03-24 17:55:37 +0100

    fixed sddm deps and theme

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -17,4 +17,10 @@
         };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtsvg
+  ];
 }
--- modules/services.nix
+++ modules/services.nix
@@ -2,4 +2,5 @@
   services.printing.enable = true;
   services.tailscale.enable = true;
   services.davfs2.enable = true;
+  services.xserver.xkb.layout = "de";
 }
