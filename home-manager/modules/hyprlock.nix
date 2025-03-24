7ebbbad1634f316d613ee183eedad90727b01f2f
AzuraAce <oliverjbmub@gmail.com>
2025-03-25 00:58:43 +0100

    fixed hyprlock config

--- home-manager/modules/hyprlock.nix
+++ home-manager/modules/hyprlock.nix
@@ -2,14 +2,16 @@
   pkgs,
   config,
   ...
-}: {
+}:
+let
+  foreground = "rgb(cdd6f4)";
+in 
+{
   programs.hyprlock = {
     enable = true;
     settings = {
-      source = "${config.home.homeDirectory}/.cache/wal/colors-hyprland.conf";
       background = [
         {
-          monitor = "";
           path = "${config.home.homeDirectory}/wallpapers/rei-stylized.jpg";
           blur_passes = 2;
           contrast = 1.0;
@@ -36,7 +38,7 @@
         dots_center = true;
         outer_color = "rgba(0, 0, 0, 0)";
         inner_color = "rgba(0, 0, 0, 0.2)";
-        font_color = "$foreground";
+        font_color = "${foreground}";
         fade_on_empty = false;
         rounding = -1;
         check_color = "rgb(204, 136, 34)";
@@ -73,7 +75,7 @@
         {
           monitor = "";
           text = "cmd[update:1000] echo \"$(/home/justin/Documents/Scripts/whatsong.sh)\"";
-          color = "$foreground";
+          color = "${foreground}";
           font_size = 18;
           font_family = "Metropolis Light, Font Awesome 6 Free Solid";
           position = "0, 50";
@@ -83,7 +85,7 @@
         {
           monitor = "";
           text = "cmd[update:1000] echo \"$USER\"";
-          color = "$foreground";
+          color = "${foreground}";
           font_size = 14;
           font_family = "JetBrains Mono";
           position = "0, -10";
@@ -93,7 +95,7 @@
         {
           monitor = "";
           text = "cmd[update:1000] echo \"$(/home/justin/Documents/Scripts/battery.sh)\"";
-          color = "$foreground";
+          color = "${foreground}";
           font_size = 24;
           font_family = "JetBrains Mono";
           position = "-90, -10";
@@ -103,35 +105,12 @@
         {
           monitor = "";
           text = "cmd[update:1000] echo \"$(/home/justin/Documents/Scripts/network-status.sh)\"";
-          color = "$foreground";
+          color = "${foreground}";
           font_size = 24;
           font_family = "JetBrains Mono";
           position = "-20, -10";
           halign = "right";
           valign = "top";
-        }
-      ];
-
-      images = [
-        {
-          monitor = "";
-          path = "/home/justin/Pictures/profile_pictures/justin_square.png";
-          size = 100;
-          border_size = 2;
-          border_color = "$foreground";
-          position = "0, -100";
-          halign = "center";
-          valign = "center";
-        }
-        {
-          monitor = "";
-          path = "/home/justin/Pictures/profile_pictures/hypr.png";
-          size = 75;
-          border_size = 2;
-          border_color = "$foreground";
-          position = "-50, 50";
-          halign = "right";
-          valign = "bottom";
         }
       ];
     };
--- home-manager/modules/swayidle.nix
+++ home-manager/modules/swayidle.nix
@@ -1,15 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   services.swayidle = {
     enable = true;
     
     timeouts = [
-      { timeout = 3; command = "swaymsg 'output * dpms off'"; resumeCommand = "swaymsg 'output * dpms on'"; }
+      { timeout = 60; command = "swaymsg 'output * dpms off'"; resumeCommand = "swaymsg 'output * dpms on'"; }
       { timeout = 90; command = "${pkgs.systemd}/bin/systemctl suspend"; }
     ];
 
     events = [
-      { event = "lock"; command = "pidof ${pkgs.hyprlock}/bin/hyprlock || ${pkgs.hyprlock}/bin/hyprlock"; }
+      { event = "lock"; command = "${config.programs.hyprlock.package}/bin/hyprlock"; }
     ];
   };
 }
