575d0d332dca2ee32f7178418e9947356480a4f2
AzuraAce <oliverjbmub@gmail.com>
2025-04-02 14:41:58 +0200

    added dwm-status; added light

--- modules/programs.nix
+++ modules/programs.nix
@@ -2,4 +2,6 @@
   programs.nix-ld.enable = true;
   programs.dconf.enable = true;
   programs.sway.enable = true;
+  # Xorg Backlight control
+  programs.light.enable = true;
 }
--- modules/services/services.nix
+++ modules/services/services.nix
@@ -6,5 +6,39 @@
     xserver.xkb.layout = "de";
     udisks2.enable = true;
     hypridle.enable = true;
+
+    # DWM Status bar
+    dwm-status = {
+      enable = true;
+      order = [
+        "audio"
+        "backlight"
+        "network"
+        "cpu_load"
+        "battery"
+        "time"
+      ];
+      extraConfig = ''
+        separator = " | "
+
+        [backlight]
+        device = "intel_backlight"
+        fallback = "amdgpu_*"
+        template = "L {BL}%"
+        icons = []
+        
+        [network]
+        no_value = "NA"
+        template = "{ESSID}"
+
+        [cpu_load]
+        template = "{CL1}"
+        update_interval = 20
+
+        [time]
+        format = "%d-%m-%Y %H:%M"
+        update_seconds = false
+      '';
+    };
   };
 }
