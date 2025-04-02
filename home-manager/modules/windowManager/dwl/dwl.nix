ba177c8a3969a0db87d9127aa6131a42f23359e1
AzuraAce <oliverjbmub@gmail.com>
2025-04-02 09:31:45 +0200

    dwm

--- home-manager/modules/windowManager/dwl/dwl.nix
+++ home-manager/modules/windowManager/dwl/dwl.nix
@@ -2,11 +2,11 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      dwl = prev.dwl.overrideAttrs ({
-        configH = ../home-manager/modules/windowManager/dwl/dwl-config.h;
-        patches = [
-          ./dwl-patches/vanitygaps-0.7.patch
-        ];
+        dwl = prev.dwl.override ({
+          configH = ./dwl-config.h;
+          patches = [
+            ./dwl-patches/vanitygaps-0.7.patch
+          ];
       });
 
     })
