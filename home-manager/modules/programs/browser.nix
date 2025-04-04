5c0fc830257890dff27a07174aa383f376768d53
AzuraAce <oliverjbmub@gmail.com>
2025-04-04 16:13:05 +0200

    added bitwarden and dark reader to firefox

--- home-manager/modules/programs/browser.nix
+++ home-manager/modules/programs/browser.nix
@@ -10,6 +10,8 @@
             Install = [
               "https://addons.mozilla.org/firefox/downloads/file/4458450/ublock_origin-1.63.2.xpi"
               "https://addons.mozilla.org/firefox/downloads/file/4424459/reddit_enhancement_suite-5.24.8.xpi"
+              "https://addons.mozilla.org/firefox/downloads/file/4440363/bitwarden_password_manager-2025.2.0.xpi"
+              "https://addons.mozilla.org/firefox/downloads/file/4439735/darkreader-4.9.103.xpi"
             ]; 
           };
         };
